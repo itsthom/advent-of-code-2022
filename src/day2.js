556125ef3bfc03ba78d4edf94cd84144aea5414f
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-02 09:13:41 -0500

    day2 part1

--- src/day2.js
+++ src/day2.js
@@ -1,8 +1,21 @@
+import { readInput } from './aoc-reader.js'
+
+function countValidPasswords (arr) {
+  return arr.filter(isValidPassword).length
+}
+
+function isValidPassword (str) {
+  const parsed = str.match(/^(?<min>\d+)-(?<max>\d+) (?<required>\w+): (?<password>.*)/).groups
+  const matched = (parsed.password.match(RegExp(parsed.required, 'g')) || []).length
+  return matched >= parseInt(parsed.min) && matched <= parseInt(parsed.max)
+}
+
 function solution () {
+  const input = readInput('day2.txt')
   return {
-    part1: '???',
+    part1: countValidPasswords(input),
     part2: '???'
   }
 }
 
-export { solution }
+export { countValidPasswords, isValidPassword, solution }
--- tests/day2.test.js
+++ tests/day2.test.js
@@ -1,7 +1,31 @@
-import { solution } from '../src/day2.js'
+import * as d2 from '../src/day2.js'
 
-describe('something', () => {
-  test('should happen!', () => {
-    expect(solution().part1).toBe('???')
+const testInput = [
+  '1-3 a: abcde',
+  '1-3 b: cdefg',
+  '2-9 c: ccccccccc'
+]
+
+describe('function: isValidPassword', () => {
+  test('input 1', () => {
+    expect(d2.isValidPassword(testInput[0])).toBe(true)
+  })
+
+  test('input 2', () => {
+    expect(d2.isValidPassword(testInput[1])).toBe(false)
+  })
+
+  test('input 3', () => {
+    expect(d2.isValidPassword(testInput[2])).toBe(true)
+  })
+
+  test(',,', () => {
+    expect(d2.isValidPassword('3-5 x: xxqxm')).toBe(true)
+  })
+})
+
+describe('function: countValidPasswords', () => {
+  test('test input', () => {
+    expect(d2.countValidPasswords(testInput)).toBe(2)
   })
 })
