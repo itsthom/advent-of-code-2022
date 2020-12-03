3d5c120f4b2cba9725d22cf8197b599a37b20971
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-03 10:10:31 -0500

    day3 part2

--- src/day3.js
+++ src/day3.js
@@ -1,20 +1,32 @@
 import { readInput } from './aoc-reader.js'
 
-function collisionReducer (total, thisRow, index) {
-  const shift = 3
-  const width = thisRow.length
+function reducerProducer (rShift, dShift) {
+  return (total, row, index) => {
+    if (index % dShift !== 0) {
+      return total
+    }
+    const pos = ((index / dShift) * rShift) % row.length
+    return row[pos] === '#' ? total + 1 : total
+  }
+}
 
-  // calculate horizontal position based on index
-  const pos = (index * shift) % width
-  return thisRow[pos] === '#' ? total + 1 : total
+function sumCollisions (input, rShift, dShift) {
+  return input.reduce(reducerProducer(rShift, dShift), 0)
 }
 
 function solution () {
   const input = readInput('day3.txt')
+  const collisions = [
+    sumCollisions(input, 1, 1),
+    sumCollisions(input, 3, 1),
+    sumCollisions(input, 5, 1),
+    sumCollisions(input, 7, 1),
+    sumCollisions(input, 1, 2)
+  ]
   return {
-    part1: input.reduce(collisionReducer, 0),
-    part2: '???'
+    part1: collisions[1],
+    part2: collisions.reduce((acc, curr) => acc * curr)
   }
 }
 
-export { collisionReducer, solution }
+export { sumCollisions, solution }
--- tests/day3.test.js
+++ tests/day3.test.js
@@ -15,7 +15,23 @@ const testInput = [
 ]
 
 describe('function: sumCollisions', () => {
-  test('with test input', () => {
-    expect(testInput.reduce(d3.collisionReducer, 0)).toBe(7)
+  test('R1 D1', () => {
+    expect(d3.sumCollisions(testInput, 1, 1)).toBe(2)
+  })
+
+  test('R3 D1', () => {
+    expect(d3.sumCollisions(testInput, 3, 1)).toBe(7)
+  })
+
+  test('R5 D1', () => {
+    expect(d3.sumCollisions(testInput, 5, 1)).toBe(3)
+  })
+
+  test('R7 D1', () => {
+    expect(d3.sumCollisions(testInput, 7, 1)).toBe(4)
+  })
+
+  test('R1 D2', () => {
+    expect(d3.sumCollisions(testInput, 1, 2)).toBe(2)
   })
 })
