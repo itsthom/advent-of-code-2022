0be23353ce4fc2914294c230f768de254e5fd250
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-04 13:11:19 -0500

    day4 part2 wip

--- src/day4.js
+++ src/day4.js
@@ -1,6 +1,6 @@
 import { readInput } from './aoc-reader.js'
 
-function isPassportValid(str) {
+function passportHasRequiredFields (str) {
   const passportObj = passportStringToObject(str)
   return [
     'byr',
@@ -9,13 +9,25 @@ function isPassportValid(str) {
     'hgt',
     'hcl',
     'ecl',
-    'pid',
+    'pid'
   ].every(x => passportObj[x])
 }
 
+function passportIsValid (str) {
+  const passportObj = passportStringToObject(str)
+  return [
+    { name: 'byr', validator: x => x >= '1920' && x <= '2002' },
+    { name: 'iyr', validator: x => x >= '2010' && x <= '2020' },
+    { name: 'eyr', validator: x => x >= '2020' && x <= '2030' },
+    { name: 'hgt', validator: x => (x >= '150cm' && x <= '193cm') || (x >= '59in' && x <= '76in') },
+    { name: 'hcl', validator: x => x.match(/#[\da-f]{6}/) },
+    { name: 'ecl', validator: x => x.match(/amb|blu|brn|gry|grn|hzl|oth/) },
+    { name: 'pid', validator: x => x.match(/\d{9}/) }
+  ].every(field => passportObj[field.name] && field.validator(passportObj[field.name]))
+}
+
 function passportStringToObject (str) {
-  return str
-    .split(/\s+/)
+  return str.split(/\s+/)
     .reduce((obj, kvp) => {
       const [key, val] = kvp.split(':')
       obj[key] = val
@@ -26,9 +38,9 @@ function passportStringToObject (str) {
 function solution () {
   const input = readInput('day4.txt', '\n\n')
   return {
-    part1: input.filter(isPassportValid).length,
-    part2: '???'
+    part1: input.filter(passportHasRequiredFields).length,
+    part2: input.filter(passportIsValid).length
   }
 }
 
-export { isPassportValid, solution }
+export { passportHasRequiredFields, passportIsValid, solution }
