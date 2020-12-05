c59303db3ac9852f984144ca8ffa13410175e34d
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-05 08:57:14 -0500

    day5 part1

--- src/day5.js
+++ src/day5.js
@@ -1,8 +1,27 @@
+import { readInput } from './aoc-reader.js'
+
+function bspFind (str, start, end) {
+  if (str.length === 0) return start
+  const [directive, ...rest] = str
+  const midpoint = Math.floor((start + end) / 2)
+  return 'FL'.includes(directive)
+    ? bspFind(rest, start, midpoint)
+    : bspFind(rest, midpoint + 1, end)
+}
+
+function calculateSeatId (str) {
+  const row = bspFind(str.substring(0, 7), 0, 127)
+  const col = bspFind(str.substring(7), 0, 7)
+  return row * 8 + col
+}
+
 function solution () {
+  const seatIds = readInput('day5.txt')
+    .map(calculateSeatId)
   return {
-    part1: '???',
+    part1: seatIds.reduce((prev, curr) => Math.max(prev, curr)),
     part2: '???'
   }
 }
 
-export { solution }
+export { calculateSeatId, solution }
--- tests/day5.test.js
+++ tests/day5.test.js
@@ -1,7 +1,24 @@
-import { solution } from '../src/day5.js'
+import * as d5 from '../src/day5.js'
 
-describe('something', () => {
-  test('should happen!', () => {
-    expect(solution().part1).toBe('???')
+const input = [
+  { str: 'FBFBBFFRLR', id: 357 },
+  { str: 'BFFFBBFRRR', id: 567 },
+  { str: 'FFFBBBFRRR', id: 119 },
+  { str: 'BBFFBBFRLL', id: 820 }
+]
+
+describe('function: calculateSeatId', () => {
+  test('using the test input', () => {
+    input.forEach((x) => {
+      expect(d5.calculateSeatId(x.str)).toBe(x.id)
+    })
+  })
+})
+
+describe('function: solution', () => {
+  test('with my test input', () => {
+    const sol = d5.solution()
+    expect(sol.part1).toBe(842)
+    expect(sol.part2).toBe('???')
   })
 })
