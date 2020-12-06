86817641382a37e50560721ca9484a9e1f39eb98
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-06 09:54:42 -0500

    day6 part2 WIP
    sample input works as expected, but it says my answer is too low on the real input

--- src/day6.js
+++ src/day6.js
@@ -4,16 +4,25 @@ const questions = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
 function countYesResponses (str) {
   return questions
-    .map(x => ({ question: x, count: (str.match(RegExp(x, 'g')) || []).length }))
-    .filter(x => x.count > 0)
+    .map(x => (str.match(RegExp(x, 'g')) || []).length)
+    .filter(x => x > 0)
+    .length
+}
+
+function countUnanimousYesResponses (str) {
+  const groupSize = str.split('\n').length
+  return questions
+    .map(x => (str.match(RegExp(x, 'g')) || []).length)
+    .filter(x => x === groupSize)
+    .length
 }
 
 function solution () {
-  const input = readInput('day6.txt', '\n\n').map(countYesResponses)
+  const input = readInput('day6.txt', '\n\n')
   return {
-    part1: input.map(x => x.length).reduce((a, b) => a + b),
-    part2: '???'
+    part1: input.map(countYesResponses).reduce((a, b) => a + b),
+    part2: input.map(countUnanimousYesResponses).reduce((a, b) => a + b)
   }
 }
 
-export { countYesResponses, solution }
+export { countYesResponses, countUnanimousYesResponses, solution }
--- tests/day6.test.js
+++ tests/day6.test.js
@@ -5,16 +5,28 @@ const input = ['abc', 'a\nb\nc', 'ab\nac', 'a\na\na\na', 'b']
 describe('function: countYesResponses', () => {
   test('sample input', () => {
     const result = input.map(d6.countYesResponses)
-    expect(result[0].length).toBe(3)
-    expect(result[1].length).toBe(3)
-    expect(result[2].length).toBe(3)
-    expect(result[3].length).toBe(1)
-    expect(result[4].length).toBe(1)
+    expect(result[0]).toBe(3)
+    expect(result[1]).toBe(3)
+    expect(result[2]).toBe(3)
+    expect(result[3]).toBe(1)
+    expect(result[4]).toBe(1)
+  })
+})
+
+describe('function: countUnanimousYesResponses', () => {
+  test('sample input', () => {
+    const result = input.map(d6.countUnanimousYesResponses)
+    expect(result[0]).toBe(3)
+    expect(result[1]).toBe(0)
+    expect(result[2]).toBe(1)
+    expect(result[3]).toBe(1)
+    expect(result[4]).toBe(1)
   })
 })
 
 describe('function: solution', () => {
   test('with my input', () => {
     expect(d6.solution().part1).toBe(6742)
+    expect(d6.solution().part2).toBeGreaterThan(3441)
   })
 })
