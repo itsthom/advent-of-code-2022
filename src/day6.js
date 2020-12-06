124a9418f3c9a124af7c2f58da3b44be735bbaee
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-06 09:30:03 -0500

    day6 part1

--- src/day6.js
+++ src/day6.js
@@ -1,8 +1,19 @@
+import { readInput } from './aoc-reader.js'
+
+const questions = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
+function countYesResponses (str) {
+  return questions
+    .map(x => ({ question: x, count: (str.match(RegExp(x, 'g')) || []).length }))
+    .filter(x => x.count > 0)
+}
+
 function solution () {
+  const input = readInput('day6.txt', '\n\n').map(countYesResponses)
   return {
-    part1: '???',
+    part1: input.map(x => x.length).reduce((a, b) => a + b),
     part2: '???'
   }
 }
 
-export { solution }
+export { countYesResponses, solution }
--- tests/day6.test.js
+++ tests/day6.test.js
@@ -1,7 +1,20 @@
-import { solution } from '../src/day6.js'
+import * as d6 from '../src/day6.js'
 
-describe('something', () => {
-  test('should happen!', () => {
-    expect(solution().part1).toBe('???')
+const input = ['abc', 'a\nb\nc', 'ab\nac', 'a\na\na\na', 'b']
+
+describe('function: countYesResponses', () => {
+  test('sample input', () => {
+    const result = input.map(d6.countYesResponses)
+    expect(result[0].length).toBe(3)
+    expect(result[1].length).toBe(3)
+    expect(result[2].length).toBe(3)
+    expect(result[3].length).toBe(1)
+    expect(result[4].length).toBe(1)
+  })
+})
+
+describe('function: solution', () => {
+  test('with my input', () => {
+    expect(d6.solution().part1).toBe(6742)
   })
 })
