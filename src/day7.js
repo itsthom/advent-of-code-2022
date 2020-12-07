0526c86831f29e1e0dbe3bc97aaac7ccb878623e
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-07 12:20:31 -0500

    day7 part1

--- src/day7.js
+++ src/day7.js
@@ -1,8 +1,26 @@
+function findGold (ruleMap, color) {
+  const rules = ruleMap.get(color)
+  return color === 'shiny gold' || rules?.some(r => findGold(ruleMap, r.color))
+}
+
+function parseBagRules (str) {
+  const [color, ...rules] = str.replace(' bags contain ', ', ').replace('.', '').split(', ')
+  return { color: color, rules: rules.map(x => x.match(/^(?<max>\d+) (?<color>.+) bag/)?.groups).filter(x => x) }
+}
+
+function toMap (input) {
+  return input.split('\n')
+    .map(parseBagRules)
+    .reduce((map, current) => map.set(current.color, current.rules), new Map())
+}
+
 function solution (input) {
+  const ruleMap = toMap(input)
+  const keys = Array.from(ruleMap.keys()).filter(x => x !== 'shiny gold')
   return {
-    part1: '???',
+    part1: keys.filter(x => findGold(ruleMap, x)).length,
     part2: '???'
   }
 }
 
-export { solution }
+export { toMap, findGold, parseBagRules, solution }
--- tests/day7.test.js
+++ tests/day7.test.js
@@ -1,10 +1,27 @@
 import { solutions, readInput } from '../allTheModules.js'
 const { mod, input } = solutions.day7
 
+const testInput =
+`light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`
+
 describe('function: solution', () => {
   test('given test input', () => {
+    const solution = mod.solution(testInput)
+    expect(solution.part1).toBe(4)
+    expect(solution.part2).toBe('???')
+  })
+
+  test('given my input', () => {
     const solution = mod.solution(readInput(input))
-    expect(solution.part1).toBe('???')
+    expect(solution.part1).toBe(144)
     expect(solution.part2).toBe('???')
   })
 })
