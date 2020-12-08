b3010cb30eefdf8799eddc6f89fe4e134b9c8931
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-08 09:57:03 -0500

    day8 part1

--- src/day8.js
+++ src/day8.js
@@ -1,6 +1,29 @@
+function execUntilLoop (instructions) {
+  let address = 0
+  let accumulator = 0
+  while (!instructions[address].visited) {
+    instructions[address].visited = true
+    switch (instructions[address].operation) {
+      case 'acc':
+        accumulator += parseInt(instructions[address].arg)
+        address++
+        break
+      case 'jmp':
+        address += parseInt(instructions[address].arg)
+        break
+      case 'nop':
+        address++
+        break
+    }
+  }
+  return accumulator
+}
+
 function solution (input) {
+  const instructions = input.split('\n').filter(x => x)
+    .map(str => str.match(/^(?<operation>\w+) (?<arg>[+-]\d+)/).groups)
   return {
-    part1: '???',
+    part1: execUntilLoop(instructions),
     part2: '???'
   }
 }
--- tests/day8.test.js
+++ tests/day8.test.js
@@ -1,10 +1,31 @@
 import { solutions, readInput } from '../allTheModules.js'
 const { mod, input } = solutions.day8
 
+const testInput =
+`nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
+
 describe('function: solution', () => {
-  test('given test input', () => {
+  describe('with test input', () => {
+    const solution = mod.solution(testInput)
+    test('part1', () => {
+      expect(solution.part1).toBe(5)
+    })
+    test('part2', () => {
+      expect(solution.part2).toBe('???')
+    })
+  })
+  describe('with real input', () => {
     const solution = mod.solution(readInput(input))
-    expect(solution.part1).toBe('???')
-    expect(solution.part2).toBe('???')
+    test('part1', () => {
+      expect(solution.part1).toBe(1610)
+    })
   })
 })
