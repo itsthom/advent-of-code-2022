4dca9d29d911544fa216372b29e6b3c8c7b772ec
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-09 10:01:52 -0500

    day9 part1

--- src/day9.js
+++ src/day9.js
@@ -1,8 +1,24 @@
-function solution (input) {
+function checkSum (arr, sum) {
+  const pairs = arr.map((val, index) => arr.slice(index + 1).map(sub => [val, sub])).flat()
+  return pairs.some(pair => pair[0] + pair[1] === sum)
+}
+
+function validateXMASData (data, preambleLength) {
+  for (let i = preambleLength; i < data.length; i++) {
+    const preamble = data.slice(i - preambleLength, i)
+    if (!checkSum(preamble, data[i])) {
+      return data[i]
+    }
+  }
+  return true
+}
+
+function solution (input, preambleLength = 25) {
+  const data = input.split('\n').map(str => parseInt(str, 10))
   return {
-    part1: '???',
+    part1: validateXMASData(data, preambleLength),
     part2: '???'
   }
 }
 
-export { solution }
+export { checkSum, validateXMASData, solution }
--- tests/day9.test.js
+++ tests/day9.test.js
@@ -1,24 +1,53 @@
 import * as day9 from '../src/day9.js'
 import { readInput } from '../reader.js'
 
+const testInput =
+`35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`
+
+describe('function: validateXMASData', () => {
+  test('with test input', () => {
+    const input = testInput.split('\n').map(str => parseInt(str, 10))
+    expect(day9.validateXMASData(input, 5)).toBe(127)
+  })
+})
+
 describe('function: solution', () => {
   describe('with test input', () => {
-    const solution = day9.solution('')
+    const solution = day9.solution(testInput, 5)
     test('part1', () => {
-      expect(solution.part1).toBe('???')
+      expect(solution.part1).toBe(127)
     })
-    test('part2', () => {
-      expect(solution.part2).toBe('???')
-    })
+    // test('part2', () => {
+    //   expect(solution.part2).toBe('???')
+    // })
   })
 
   describe('with real input', () => {
     const solution = day9.solution(readInput('day9.txt'))
     test('part1', () => {
-      expect(solution.part1).toBe('???')
+      expect(solution.part1).toBe(21806024)
     })
-    test('part2', () => {
-      expect(solution.part2).toBe('???')
-    })
+    // test('part2', () => {
+    //   expect(solution.part2).toBe('???')
+    // })
   })
 })
