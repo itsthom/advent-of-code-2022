7da6f3ed3c9ab1f1c83754b3bb1bf994c88eef4f
Tom Gleghorn <tgleghorn@covermymeds.com>
2020-12-10 09:13:02 -0500

    day10 part1

--- src/day10.js
+++ src/day10.js
@@ -1,8 +1,25 @@
+function countSteps (joltages) {
+  // sort & add the outlet and device joltages
+  const sorted = joltages.sort((a, b) => a - b)
+  sorted.unshift(0)
+  sorted.push(sorted[sorted.length - 1] + 3)
+
+  return sorted.reduce((result, current, index, arr) => {
+    if (index > 0) {
+      if (current - arr[index - 1] === 1) result.ones++
+      if (current - arr[index - 1] === 3) result.threes++
+    }
+    return result
+  }, { ones: 0, threes: 0 })
+}
+
 function solution (input) {
+  const joltages = input.split('\n').map(Number)
+  const steps = countSteps(joltages)
   return {
-    part1: '???',
+    part1: steps.ones * steps.threes,
     part2: '???'
   }
 }
 
-export { solution }
+export { countSteps, solution }
--- tests/day10.test.js
+++ tests/day10.test.js
@@ -1,11 +1,67 @@
 import * as day10 from '../src/day10.js'
 import { readInput } from '../reader.js'
 
+const input1 =
+`16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`
+
+const input2 =
+`28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3`
+
 describe('function: solution', () => {
-  describe('with test input', () => {
-    const solution = day10.solution('')
+  describe('with test input 1', () => {
+    const solution = day10.solution(input1)
     test('part1', () => {
-      expect(solution.part1).toBe('???')
+      expect(solution.part1).toBe(35)
+    })
+    test('part2', () => {
+      expect(solution.part2).toBe('???')
+    })
+  })
+
+  describe('with test input 2', () => {
+    const solution = day10.solution(input2)
+    test('part1', () => {
+      expect(solution.part1).toBe(220)
     })
     test('part2', () => {
       expect(solution.part2).toBe('???')
@@ -13,12 +69,12 @@ describe('function: solution', () => {
   })
 
   describe('with real input', () => {
-    const solution = day10.solution(readInput('day10.txt'))
-    test('part1', () => {
-      expect(solution.part1).toBe('???')
-    })
-    test('part2', () => {
-      expect(solution.part2).toBe('???')
-    })
+    // const solution = day10.solution(readInput('day10.txt'))
+    // test('part1', () => {
+    //   expect(solution.part1).toBe(2046)
+    // })
+    // test('part2', () => {
+    //   expect(solution.part2).toBe('???')
+    // })
   })
 })
