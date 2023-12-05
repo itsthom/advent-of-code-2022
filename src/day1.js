a13d7edd6939800a4aa43c2203fefc48668211ba
Tom Gleghorn <tgleghorn@covermymeds.com>
2023-12-05 16:14:08 -0500

    d1p2 WIP

--- src/day1.js
+++ src/day1.js
@@ -6,11 +6,34 @@ function calibrationValue (input) {
     .reduce((a, b) => a + b, 0)
 }
 
+function strToDigitChar (str) {
+  const mapping = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9'
+  }
+  return mapping[str] || str
+}
+
+function calibrationValueUpdated (input) {
+  return input.split('\n')
+    .map(s => s.match(/(\d|one|two|three|four|five|six|seven|eight|nine)/g))
+    .filter(a => a !== null)
+    .map(a => parseInt(strToDigitChar(a[0]) + strToDigitChar(a[a.length - 1]), 10))
+    .reduce((a, b) => a + b, 0)
+}
+
 function solution (input) {
   return {
     part1: calibrationValue(input),
-    part2: '???'
+    part2: calibrationValueUpdated(input)
   }
 }
 
-export { calibrationValue, solution }
+export { calibrationValue, calibrationValueUpdated, solution }
--- tests/day1.test.js
+++ tests/day1.test.js
@@ -1,12 +1,26 @@
 import * as day1 from '../src/day1.js'
 
 describe('calibrationValue', () => {
-  test('first sample elf', () => {
+  test('first sample data', () => {
     const input =
     `1abc2
     pqr3stu8vwx
     a1b2c3d4e5f
     treb7uchet`
     expect(day1.calibrationValue(input)).toBe(142)
+  })
+})
+
+describe('calibrationValueUpdated', () => {
+  test('second sample data', () => {
+    const input =
+    `two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen`
+    expect(day1.calibrationValueUpdated(input)).toBe(281)
   })
 })
